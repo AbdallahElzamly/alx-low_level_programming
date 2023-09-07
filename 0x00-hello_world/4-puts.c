f558c509b80c768df28030f1688bfce63f28dd28
AbdallahElzamly <56141320+AbdallahElzamly@users.noreply.github.com>
2023-09-07 11:22:45 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main(void) /*warning: no description found for function main*/
+int main(void)//warning: no description found for function main
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
