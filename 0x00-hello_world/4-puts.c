1e93c504e91440b827089c07b513cda6b2cec45a
AbdallahElzamly <56141320+AbdallahElzamly@users.noreply.github.com>
2023-09-07 20:19:32 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Description: A C program that prints with put function.
+  *
+  * Return: Always 0 (Success)
+*/
+
 int main(void) 
-//warning: no description found for function main
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
