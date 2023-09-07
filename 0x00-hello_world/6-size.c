5d8b380958ff04e3437ff839e0431052597e23e0
AbdallahElzamly <56141320+AbdallahElzamly@users.noreply.github.com>
2023-09-07 22:39:51 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@
 
 int main(void)
 {
-pritnf("Size of a char: %lu byte(s)\n", sizeof(char));
-pritnf("Size of an int: %lu byte(s)\n", sizeof(int));
-pritnf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-pritnf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-pritnf("Size of a float: %lu byte(s)\n", sizeof(float));
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
