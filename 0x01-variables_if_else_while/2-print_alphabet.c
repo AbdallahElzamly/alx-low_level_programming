8ad4a66cb6a32574eb42a7f5da2a99b894813a11
AbdallahElzamly <abdallahelzamly2030.com>
2023-09-08 10:19:56 -0700

    mm

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+  * main - Entry point
+  *
+  * Description: Print all lowercase alphabet
+  *
+  * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
