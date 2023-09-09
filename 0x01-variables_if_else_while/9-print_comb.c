d262b40fdb378dd858ce93e20542b6d98e6d22a0
AbdallahElzamly <abdallahelzamly2030.com>
2023-09-08 18:23:38 -0700

    . .

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+  * main - Entry point
+  *
+  * Description: Print all lowercase and upper case alphabet
+  *
+  * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + 48);
+		if (i != 9)
+		{
+		putchar(",");
+		putchar(" ");
+		}
+		i++;
+	}
+	putchar("\n");
+	return (0);
+}
+
