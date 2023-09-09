193188fbcefc3e7f1a43f9979a371b8a312c2284
AbdallahElzamly <abdallahelzamly2030.com>
2023-09-08 20:49:59 -0700

    . .

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+  * main - Entry point
+  *
+  * Description: Print all lowercase and upper case alphabet
+  *
+  * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int d1 = 0, d2;
+
+	while (d1 < 9)
+	{
+		d2 = 1;
+		while (d2 < 10)
+		{
+			if (d1 < d2 && d1 != d2)
+			{
+				putchart(d1 + 48);
+				putchart(d2 + 48);
+			}
+			if (d1 + d2 != 17)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			d2++;
+		}
+
+		d1++;
+	}
+	return (0);
+}
+
