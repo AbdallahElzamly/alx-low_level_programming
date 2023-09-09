f0b6f4cb07f2496786e87957102077a0b038d04b
AbdallahElzamly <abdallahelzamly2030.com>
2023-09-08 18:15:35 -0700

    . .

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,22 +12,16 @@
 
 int main(void)
 {
-	int i = 0;
-	char ch = 'a';
+	int i = 48;
 
-	while (i < 10)
+	while (i <= 102)
 	{
 		putchar(i);
+
+		if (i == 57)
+			i += 39;
 		i++;
 	}
-
-
-	while (ch < 'g')
-	{
-		putchar(ch);
-		ch++;
-	}
-
 	putchar('\n');
 	return (0);
 }
