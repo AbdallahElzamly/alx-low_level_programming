a0d05fb2bc5437cf8eb25e225d3c781ff782bf11
AbdallahElzamly <abdallahelzamly2030.com>
2023-09-08 18:06:13 -0700

    . .

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+  * main - Entry point
+  *
+  * Description: Print all lowercase and upper case alphabet
+  *
+  * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar("%i", i);
+		i++;
+	}
+	char ch = 'a';
+
+	while (ch < 'g')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	putchar('\n');
+	return (0);
+}
