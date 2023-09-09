20253feb2c83948b4dc60d530b835f514e8c7c92
AbdallahElzamly <abdallahelzamly2030.com>
2023-09-08 17:56:40 -0700

    . .

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+  * main - Entry point
+  *
+  * Description: Print all lowercase and upper case alphabet
+  *
+  * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch ='z';
+	while (ch >= 'a' )
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	
+	return (0);
+}
