db64754d327bac33cf14d61a389e24a51d24e9f0
AbdallahElzamly <abdallahelzamly2030.com>
2023-09-12 12:56:27 -0700

    . .

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,15 +9,13 @@
  * Return: last digit
 */
 
-int print_last_digit (int n)
+int print_last_digit(int n)
 {
-    
-    int last;
-    
-    last = n % 10;
-    if (last < 0)
-        last *= -1;
-    _putchar (last + '0');
-    return (last);
-}
+	int last;
 
+	last = n % 10;
+	if (last < 0)
+		last *= -1;
+	_putchar (last + '0');
+	return (last);
+}
