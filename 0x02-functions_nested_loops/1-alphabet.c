b22370f105830656bfa27a1e329ae1e75936813e
AbdallahElzamly <abdallahelzamly2030.com>
2023-09-12 09:58:32 -0700

    . .

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alpphabet - utilitzes on the _putchar funtion to print
+ * the alphabet a - x
+ *
+ */
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
