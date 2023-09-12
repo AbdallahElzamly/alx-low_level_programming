e7dbd07508572d334cbc5343f82c8cdf5cd95ced
AbdallahElzamly <abdallahelzamly2030.com>
2023-09-12 11:50:52 -0700

    . .

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -13,16 +13,7 @@
 
 int _abs(int n)
 {
-	if (n > 0)
-	{
-		return (1);
-	}
-	else if (n < 0)
-	{
-		return (n * -1);
-	}
-	else
-	{
-		return (0);
-	}
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
 }
