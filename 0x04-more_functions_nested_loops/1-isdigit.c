04f921247fe5bb51a1e1f1979185d4ddd9eb93bd
AbdallahElzamly <abdallahelzamly2030.com>
2023-09-14 17:13:56 -0700

    . .

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - is this is a digit?
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
