e5a96d2d6b9995acc24b73c18e8dfa1ca055f9d1
AbdallahElzamly <abdallahelzamly2030.com>
2023-09-18 00:16:11 -0700

    . .

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,15 +7,22 @@
 void rev_string(char *s)
 {
 	char tmp;
-	int i, len = 0;
+	int i, len, len1;
 
-	while (s[len])
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
+	{
 		len++;
+	}
 
-	for (i = 0; i < (len - 1) / 2; i++)
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
 	{
 		tmp = s[i];
-		s[i] = s[len];
-		s[len--] = tmp;
+		s[i] = s[len1];
+		s[len1--] = tmp;
 	}
 }
