8d1c7c71b29d1671ae8c1280895b9b3ab531e7ef
AbdallahElzamly <abdallahelzamly2030.com>
2023-10-05 19:31:32 +0300

    . .

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,11 +10,11 @@
 
 char *create_array(unsigned int size, char c)
 {
-    char *n = malloc(size);
+	char *n = malloc(size);
 
-    if (size == 0 || n == 0)
-        return (0);
-    while (size--)
-        n[size] = c;
-    return (n);
+	if (size == 0 || n == 0)
+		return (0);
+	while (size--)
+		n[size] = c;
+	return (n);
 }
