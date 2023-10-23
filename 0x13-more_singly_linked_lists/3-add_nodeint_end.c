ff0c3ea0209f2b6d357eaaebd7702c75db5f362f
AbdallahElzamly <abdallahelzamly2030.com>
2023-10-23 20:52:37 +0300

    . .

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,8 +11,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
     listint_t *new_node = malloc(sizeof(listint_t));
 	listint_t *node;
 
-    if (!head || !new_node)
-        return (NULL);
+    if (new_node == NULL)
+		return (NULL);
 
 	new_node->next = NULL;
 	new_node->n = n;
