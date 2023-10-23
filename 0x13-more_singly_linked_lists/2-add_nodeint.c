06a65d07a44319280227af2205b588972b27268f
AbdallahElzamly <abdallahelzamly2030.com>
2023-10-23 20:03:40 +0300

    . .

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,10 +12,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
     if(head == NULL || new_node == NULL)
         return(NULL);
-    
+
     new_node->n = n;
     new_node->next = *head;
     *head = new_node;
-    
+
 	return (new_node);
 }
