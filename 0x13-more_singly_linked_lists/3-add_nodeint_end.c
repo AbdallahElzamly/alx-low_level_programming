b7e8112f9729ad540679b49634b840db1c36f4ff
AbdallahElzamly <abdallahelzamly2030.com>
2023-10-23 20:56:22 +0300

    . .

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,25 +8,56 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *new_node = malloc(sizeof(listint_t));
-	
-	listint_t *node;
+	listint_t *new_node, *tmp_node;
 
-    if (new_node == NULL)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 		return (NULL);
 
-	new_node->next = NULL;
 	new_node->n = n;
+	new_node->next = NULL;
 	if (*head == NULL)
 	{
 		*head = new_node;
 	}
 	else
 	{
-		node = *head;
-		while (node->next != NULL)
-			node = node->next;
-		node->next = new_node;
+		tmp_node = *head;
+		while (tmp_node->next != NULL)
+			tmp_node = tmp_node->next;
+		tmp_node->next = new_node;
 	}
 	return (new_node);
 }
+// #include "lists.h"
+
+// /**
+//  * add_nodeint_end - Adds a node at the end of a listint_t.
+//  * @n: data type pointer of struct
+//  * @head: data type pointer of pointer the head/next node
+//  * Return: elements of the str new_node
+//  */
+// listint_t *add_nodeint_end(listint_t **head, const int n)
+// {
+//     listint_t *new_node = malloc(sizeof(listint_t));
+	
+// 	listint_t *node;
+
+//     if (new_node == NULL)
+// 		return (NULL);
+
+// 	new_node->next = NULL;
+// 	new_node->n = n;
+// 	if (*head == NULL)
+// 	{
+// 		*head = new_node;
+// 	}
+// 	else
+// 	{
+// 		node = *head;
+// 		while (node->next != NULL)
+// 			node = node->next;
+// 		node->next = new_node;
+// 	}
+// 	return (new_node);
+// }
