427c8c8af15078ab200db02874535e0e020b1e2a
AbdallahElzamly <abdallahelzamly2030.com>
2023-10-24 14:49:51 +0300

    ...

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,14 +1,14 @@
 nclude "lists.h"
 
 /**
- *  * listint_len - returns the number of elements in
- *   * 
- *    * a linked list.
- *     * 
- *      * @h: head of a list.
- *       *
- *        * Return: numbers of nodes.
- *         */
+ * listint_len - returns the number of elements in
+ * 
+ * a linked list.
+ * 
+ * @h: head of a list.
+ *
+ * Return: numbers of nodes.
+ **/
 size_t listint_len(const listint_t *h)
 {
     size_t nnodes = 0;
